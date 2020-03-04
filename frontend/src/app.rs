6bbf981cb6b777bb7fd68451ce7a4f9546de255f
Konstantin Petrov <k.petrov@petrel.ai>
2020-03-05 00:39:51 +0600

    Remove tested routes

--- frontend/src/app.rs
+++ frontend/src/app.rs
@@ -8,10 +8,6 @@ pub struct App;
 enum AppRouter {
     #[to = "/!"]
     RootPath,
-    #[to = "/hello!"]
-    HelloPath,
-    #[to = "/awesome!"]
-    AwesomePath,
     #[to = "/page-not-found"]
     PageNotFound(Permissive<String>),
 }
@@ -36,9 +32,7 @@ impl Component for App {
                     <Router<AppRouter, ()>
                         render = Router::render(|switch: AppRouter | {
                             match switch {
-                                AppRouter::RootPath => html!{<h2>{"this is root"}</h2>},
-                                AppRouter::HelloPath => html!{<h2>{"Hello world"}</h2>},
-                                AppRouter::AwesomePath => html!{<h2>{"My awesome Yew with Yew-Router and Parcel application!"}</h2>},
+                                AppRouter::RootPath => html!{<h2>{"Home"}</h2>},
                                 AppRouter::PageNotFound(Permissive(None)) => html!{"Page not found"},
                                 AppRouter::PageNotFound(Permissive(Some(missed_route))) => html!{format!("Page '{}' not found", missed_route)}
                             }
